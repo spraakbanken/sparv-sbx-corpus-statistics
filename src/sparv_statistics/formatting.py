7067088c9fc2c20ee895592a776e4c491dd35f47
Kristoffer Andersson <kristoffer.andersson@gu.se>
2025-06-16 14:50:39 +0200

    test: add doc-tests

--- src/sparv_statistics/formatting.py
+++ src/sparv_statistics/formatting.py
@@ -28,7 +28,17 @@ _SIGN_2: int = 10
 
 
 def fmt_number_signific(number: float | int, significant_digits: int = 3) -> str:
-    """Format a number with given number of significant digits with locale."""
+    """Format a number with given number of significant digits with locale.
+
+    >>> fmt_number_signific(0.123456789)
+    '0.123'
+    >>> fmt_number_signific(1.23456789)
+    '1.23'
+    >>> fmt_number_signific(12.3456789)
+    '12.3'
+    >>> fmt_number_signific(123.456789)
+    '123'
+    """
     if number >= _SIGN_3:
         significant_digits -= 3
     elif number >= _SIGN_2:
@@ -45,7 +55,15 @@ def fmt_number_signific(number: float | int, significant_digits: int = 3) -> str
 
 
 def fmt_number_decimals(number: float | int, n_decimals: int) -> str:
-    """Format a number with given number decimals with locale."""
+    """Format a number with given number decimals with locale.
+
+    >>> fmt_number_decimals(0.1, 0)
+    '< 1'
+    >>> fmt_number_decimals(0.1, 1)
+    '0.1'
+    >>> fmt_number_decimals(0.01, 1)
+    '< 0.1'
+    """
     try:
         limit = _PERCENT_LIMIT_MAP[n_decimals]
     except KeyError:
